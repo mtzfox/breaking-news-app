1b3a10905e0634036157d8fdb7aa0c1e756039e5
Mike <21209421+mtzfox@users.noreply.github.com>
2022-12-02 03:45:52 -0800

    updated config and depdencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()], 
-  build: {
-    outDir: 'docs'
-  },
-  base: '/breaking-news-app/'
-})
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      mode: 'development',
+      base: '/'
+    }
+  } else {
+    // command === 'build'
+    return {
+      plugins: [react()], 
+      mode: 'production',
+      build: {
+        outDir: 'docs'
+      },
+      base: '/breaking-news-app/'
+    }
+  }
+});
+
+
